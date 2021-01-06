a75d077acc46479060214bc50fd055a68e62f0b0
liujianfei <cnliujianfei@163.com>
2021-01-06 21:34:31 +0800

    add secs lib

---
+++ src/main/java/com/example/secs4j/SecsEquipmentCommunicationApplication.java
@@ -0,0 +1,8 @@
+package com.example.secs4j;
+
+import com.ias.application.chat.ChatApplication;
+public class SecsEquipmentCommunicationApplication {
+    public static void main(String[] args) {
+        ChatApplication.main(args);
+    }
+}
---
+++ src/test/java/com/ias/isecs/IsecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ias.isecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsecsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
